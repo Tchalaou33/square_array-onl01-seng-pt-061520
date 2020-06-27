7a038eccefc5a70b8c1b131ba6909738023c5dca
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 19:17:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
- def square_array(array)
-  newArr= []
-  array.each do |number|
-  puts "I like #{number}."
-  return newArr << number **2
-end
-end
+# def square_array(array)
+#   newArr= []
+#   array.each do |number|
+#   puts "I like #{number}."
+#   return newArr << number **2
+# end
+# end
 
+def square_array(array)
+	  arr = []
+  array.each { |num| arr << num ** 2}
+  return arr
+end
